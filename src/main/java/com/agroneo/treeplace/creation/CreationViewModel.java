a4727edca72de3cef0d857e089dcf959acf8e4c8
pagetronic <pagetronic@gmail.com>
2019-12-14 14:06:28 +0400

    start project

---
+++ src/main/java/com/agroneo/treeplace/creation/CreationViewModel.java
@@ -0,0 +1,19 @@
+package com.agroneo.treeplace.creation;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class CreationViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public CreationViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is creation fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
