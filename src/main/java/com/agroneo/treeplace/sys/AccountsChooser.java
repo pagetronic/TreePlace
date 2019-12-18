57fd4e494e6382ae677ff620daeeb3878a573429
pagetronic <pagetronic@gmail.com>
2019-12-18 13:21:09 +0400

    complete externalize account chooser

--- src/main/java/com/agroneo/treeplace/sys/AccountsChooser.java
+++ src/main/java/com/agroneo/treeplace/sys/AccountsChooser.java
@@ -4,6 +4,7 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
@@ -19,11 +20,15 @@ import com.agroneo.treeplace.auth.AuthActivity;
 import com.agroneo.treeplace.auth.AuthService;
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AccountsChooser {
 
     public static void make(final Activity activity) {
 
         ImageView avatar = activity.findViewById(R.id.avatar);
+
         final String account_name = AuthService.getAccountNameActive(activity);
 
         if (account_name != null) {
@@ -36,80 +41,88 @@ public class AccountsChooser {
             }
         }
 
-        final Account[] accounts = AccountManager.get(activity).getAccountsByType(activity.getResources().getString(R.string.account_type));
-        if (accounts.length > 0) {
 
-            final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final Account[] accounts = AccountManager.get(activity).getAccountsByType(activity.getResources().getString(R.string.account_type));
+                if (accounts.length > 0) {
+
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
 
 
-            builder.setTitle(R.string.select_account);
-            final LayoutInflater inflater = activity.getLayoutInflater();
-            View convertView = inflater.inflate(R.layout.accounts, null);
-            builder.setView(convertView);
-            builder.setCancelable(true);
-            final AlertDialog dialog = builder.show();
-            ListView list = convertView.findViewById(R.id.accounts);
-            list.setAdapter(new BaseAdapter() {
-                @Override
-                public int getCount() {
-                    return accounts.length + 1;
-                }
-
-                @Override
-                public Object getItem(int position) {
-                    return accounts.length < position ? accounts[position] : null;
-                }
-
-                @Override
-                public long getItemId(int position) {
-                    return -1;
-                }
-
-                @Override
-                public View getView(final int position, View convertView, ViewGroup parent) {
-
-                    View view = inflater.inflate(R.layout.accounts_item, null);
-
-                    view.setOnClickListener(new View.OnClickListener() {
+                    builder.setTitle(R.string.select_account);
+                    final LayoutInflater inflater = activity.getLayoutInflater();
+                    View convertView = inflater.inflate(R.layout.accounts, null);
+                    builder.setView(convertView);
+                    builder.setCancelable(true);
+                    final AlertDialog dialog = builder.show();
+                    ListView list = convertView.findViewById(R.id.accounts);
+                    list.setAdapter(new BaseAdapter() {
                         @Override
-                        public void onClick(View v) {
+                        public int getCount() {
+                            return accounts.length + 1;
+                        }
+
+                        @Override
+                        public Object getItem(int position) {
+                            return accounts.length < position ? accounts[position] : null;
+                        }
+
+                        @Override
+                        public long getItemId(int position) {
+                            return -1;
+                        }
+
+                        @Override
+                        public View getView(final int position, View convertView, ViewGroup parent) {
+
+                            View view = inflater.inflate(R.layout.accounts_item, null);
+
+                            view.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    if (position >= accounts.length) {
+                                        activity.startActivityForResult(new Intent(activity, AuthActivity.class), 1);
+                                        dialog.cancel();
+                                    } else {
+                                        AuthService.setAccountActive(activity, accounts[position].name);
+                                        dialog.cancel();
+                                        activity.recreate();
+                                    }
+                                }
+                            });
+
+                            ImageView avatar = view.findViewById(R.id.avatar);
+                            TextView text = view.findViewById(R.id.name);
+                            TextView email = view.findViewById(R.id.email);
+
                             if (position >= accounts.length) {
-                                activity.startActivityForResult(new Intent(activity, AuthActivity.class), 1);
-                                dialog.cancel();
-                            } else {
-                                AuthService.setAccountActive(activity, accounts[position].name);
-                                dialog.cancel();
-                                activity.recreate();
+                                avatar.setVisibility(View.INVISIBLE);
+                                email.setVisibility(View.INVISIBLE);
+                                text.setText(R.string.new_account);
+                                return view;
                             }
+
+                            text.setText(AuthService.getAccountData(activity, accounts[position].name, "name"));
+                            email.setText(accounts[position].name);
+                            String logo = AuthService.getAccountData(activity, accounts[position].name, "avatar");
+
+                            Glide.with(activity).load(Uri.parse(logo + "@" + avatar.getWidth()))
+                                    .error(R.drawable.logo)
+                                    .circleCrop()
+                                    .into(avatar);
+
+                            return view;
                         }
                     });
-
-                    ImageView avatar = view.findViewById(R.id.avatar);
-                    TextView text = view.findViewById(R.id.name);
-                    TextView email = view.findViewById(R.id.email);
-
-                    if (position >= accounts.length) {
-                        avatar.setVisibility(View.INVISIBLE);
-                        email.setVisibility(View.INVISIBLE);
-                        text.setText(R.string.new_account);
-                        return view;
-                    }
-
-                    text.setText(AuthService.getAccountData(activity, accounts[position].name, "name"));
-                    email.setText(accounts[position].name);
-                    String logo = AuthService.getAccountData(activity, accounts[position].name, "avatar");
-
-                    Glide.with(activity).load(Uri.parse(logo + "@" + avatar.getWidth()))
-                            .error(R.drawable.logo)
-                            .circleCrop()
-                            .into(avatar);
-
-                    return view;
+                } else {
+                    activity.startActivityForResult(new Intent(activity, AuthActivity.class), 1);
                 }
-            });
-        } else {
-            activity.startActivityForResult(new Intent(activity, AuthActivity.class), 1);
-        }
+            }
+        });
+
     }
 
 }
