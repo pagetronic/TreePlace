33572b30e7d656bf8529db23d9f2fd31b9291ed8
pagetronic <pagetronic@gmail.com>
2019-12-24 18:44:30 +0400

    abortable api request

--- src/main/java/com/agroneo/treeplace/api/ApiAsync.java
+++ src/main/java/com/agroneo/treeplace/api/ApiAsync.java
@@ -9,68 +9,109 @@ public class ApiAsync extends AsyncTask<Object, Integer, ApiResponse> {
 
 
     private ApiResult func;
-    private String access_token;
+    private ApiRequest req;
 
-    private ApiAsync(ApiResult func, String access_token) {
-        this.access_token = access_token;
+    private ApiAsync(ApiRequest req, ApiResult func) {
         this.func = func;
+        this.req = req;
     }
 
-    public static void post(final Context ctx, final String url, final Json data, final ApiResult func) {
+    public static ApiRequest post(final Context ctx, final String url, final Json data, final ApiResult func) {
+        final ApiRequest req = new ApiRequest(ctx, url);
         Accounts.getAccessToken(ctx, new Accounts.Token() {
             @Override
             public void get(final String access_token) {
-                post(access_token, ctx, url, data, func);
+                if (!req.isAbort()) {
+                    req.setAuthToken(access_token);
+                    post(req, data, func);
+                }
             }
         });
+        return req;
     }
 
-    public static void post(final String access_token, final Context ctx, final String url, final Json data, final ApiResult func) {
+    public static ApiRequest post(final String access_token, final Context ctx, final String url, final Json data, final ApiResult func) {
+        ApiRequest req = new ApiRequest(ctx, url);
+        req.setAuthToken(access_token);
+        post(req, data, func);
+        return req;
+    }
 
-        new ApiAsync(new ApiResult() {
+    private static void post(final ApiRequest req, final Json data, final ApiResult func) {
+
+        new ApiAsync(req, new ApiResult() {
 
             @Override
             public void success(Json data) {
+                if (req.isAbort()) {
+                    return;
+                }
                 func.success(data);
+
             }
 
             @Override
             public void error(int code, Json data) {
+                if (req.isAbort()) {
+                    return;
+                }
                 if (code == 401 && "EXPIRED_ACCESS_TOKEN".equals(data.getString("error"))) {
-                    Accounts.invalidateAuthToken(ctx, access_token);
-                    post(ctx, url, data, func);
+                    req.invalidateAuthToken();
+                    post(req, data, func);
                     return;
                 }
                 func.error(code, data);
             }
-        }, access_token).execute("POST", url, data);
+        }).execute("POST", data);
 
     }
 
-    public static void get(final Context ctx, final String url, final ApiResult func) {
+    public static ApiRequest get(final Context ctx, final String url, final ApiResult func) {
 
+        final ApiRequest req = new ApiRequest(ctx, url);
         Accounts.getAccessToken(ctx, new Accounts.Token() {
             @Override
             public void get(final String access_token) {
-                ApiAsync.get(access_token, ctx, url, func);
+
+                req.setAuthToken(access_token);
+                if (!req.isAbort()) {
+                    ApiAsync.get(req, ctx, url, func);
+                }
             }
         });
+        return req;
     }
 
-    public static void get(final String access_token, final Context ctx, final String url, final ApiResult func) {
+    public static ApiRequest get(final String access_token, final Context ctx, final String url, final ApiResult func) {
 
-        new ApiAsync(new ApiResult() {
+        final ApiRequest req = new ApiRequest(ctx, url);
+        req.setAuthToken(access_token);
+        if (!req.isAbort()) {
+            ApiAsync.get(req, ctx, url, func);
+        }
+        return req;
+    }
+
+    private static void get(final ApiRequest req, final Context ctx, final String url, final ApiResult func) {
+
+        new ApiAsync(req, new ApiResult() {
 
             @Override
             public void success(Json data) {
+                if (req.isAbort()) {
+                    return;
+                }
                 func.success(data);
             }
 
             @Override
             public void error(int code, Json data) {
+                if (req.isAbort()) {
+                    return;
+                }
                 switch (data.getString("error")) {
                     case "EXPIRED_ACCESS_TOKEN":
-                        Accounts.invalidateAuthToken(ctx, access_token);
+                        req.invalidateAuthToken();
                         post(ctx, url, data, func);
                         break;
                     case "AUTHORIZATION_SCOPE_ERROR":
@@ -82,21 +123,23 @@ public class ApiAsync extends AsyncTask<Object, Integer, ApiResponse> {
                         break;
                 }
             }
-        }, access_token).execute("GET", url);
-
+        }).execute("GET", url);
     }
 
     @Override
     protected ApiResponse doInBackground(Object... params) {
         if (params[0].equals("POST")) {
-            return ApiSync.post(access_token, (String) params[1], (Json) params[2]);
+            return req.post((Json) params[1]);
         } else {
-            return ApiSync.get(access_token, (String) params[1]);
+            return req.get();
         }
     }
 
     @Override
     protected void onPostExecute(ApiResponse rez) {
+        if (req.isAbort()) {
+            return;
+        }
 
         if (func == null) {
             return;
---
+++ src/main/java/com/agroneo/treeplace/api/ApiRequest.java
@@ -0,0 +1,114 @@
+package com.agroneo.treeplace.api;
+
+import android.accounts.AccountManager;
+import android.content.Context;
+
+import com.agroneo.treeplace.BuildConfig;
+import com.agroneo.treeplace.R;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class ApiRequest {
+
+    private static final String API_HOST = BuildConfig.DEBUG ? "http://api.agroneo.com" : "https://api.agroneo.com";
+    private Context ctx;
+    private HttpURLConnection connection;
+    private boolean abort = false;
+    private String access_token = null;
+
+    public ApiRequest(Context ctx, String path) {
+        this.ctx = ctx;
+        try {
+            connection = (HttpURLConnection) new URL(API_HOST + path).openConnection();
+            connection.setRequestProperty("User-Agent", "Agroneo TreePlace Android V3");
+            connection.setRequestProperty("Accept", "application/json");
+
+            if (BuildConfig.DEBUG) {
+                connection.setReadTimeout(Integer.MAX_VALUE);
+                connection.setConnectTimeout(Integer.MAX_VALUE);
+            } else {
+                connection.setReadTimeout(15000);
+                connection.setConnectTimeout(15000);
+            }
+            connection.setUseCaches(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setAuthToken(String access_token) {
+        if (access_token != null) {
+            this.access_token = access_token;
+            connection.setRequestProperty("Authorization", "Bearer " + access_token);
+        }
+    }
+
+    public ApiResponse post(Json data) {
+
+        try {
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            OutputStream os = connection.getOutputStream();
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, StandardCharsets.UTF_8));
+            writer.write(data.toString(true));
+            writer.flush();
+            writer.close();
+            String response = IOUtils.toString(connection.getResponseCode() != HttpURLConnection.HTTP_OK ? connection.getErrorStream() : connection.getInputStream());
+            connection.disconnect();
+            os.close();
+            return new ApiResponse(connection.getResponseCode(), response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ApiResponse get() {
+
+        try {
+
+            connection.setRequestMethod("GET");
+
+            String response = IOUtils.toString(connection.getResponseCode() != 200 ? connection.getErrorStream() : connection.getInputStream());
+            connection.disconnect();
+            return new ApiResponse(connection.getResponseCode(), response);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public void abort() {
+        abort = true;
+        try {
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public boolean isAbort() {
+        return abort;
+    }
+
+    public void invalidateAuthToken() {
+        if (access_token != null) {
+            AccountManager am = AccountManager.get(ctx);
+            am.invalidateAuthToken(ctx.getString(R.string.account_type), access_token);
+        }
+    }
+}
