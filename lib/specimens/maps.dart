b2e7d317c6831f62063c1ee34137c20f05cf8004
pagetronic <pagetronic@gmail.com>
2020-01-03 19:22:40 +0400

    icon size bug from google

--- lib/specimens/maps.dart
+++ lib/specimens/maps.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
-import 'dart:ui';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class SpecimensMap extends StatefulWidget {
@@ -41,10 +43,9 @@ class SpecimensMapState extends State<SpecimensMap> {
   }
 
   void setMarker(LatLng latLng, dynamic json) async {
-    BitmapDescriptor icon =
-        await BitmapDescriptor.fromAssetImage(createLocalImageConfiguration(context, size: Size(30, 30)), 'assets/marker.png');
     Marker marker = Marker(
-      icon: icon,
+      icon: BitmapDescriptor.fromBytes(
+          await getBytesFromAsset('assets/marker.png', 64)),
       markerId: new MarkerId("xxxxx"),
       position: latLng,
       onTap: () {
@@ -55,5 +56,15 @@ class SpecimensMapState extends State<SpecimensMap> {
     setState(() {
       markers.add(marker);
     });
+  }
+
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
+        .buffer
+        .asUint8List();
   }
 }
