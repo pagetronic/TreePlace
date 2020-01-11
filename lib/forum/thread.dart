7b932b79079563f5e74b29796c8fca17c2adda4b
pagetronic <pagetronic@gmail.com>
2020-01-11 16:31:42 +0400

    form reply in last

--- lib/forum/thread.dart
+++ lib/forum/thread.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:agroneo_treeplace/api/list.dart';
+import 'package:agroneo_treeplace/ui/inputs.dart';
 import 'package:agroneo_treeplace/ui/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -22,43 +25,57 @@ class ThreadViewState extends BaseState<ThreadView> {
         title: Text(args['title']),
         actions: <Widget>[connector],
       ),
-      body: ListApi.get("/threads/" + args['id'], (json) {
-        return postTile(context, theme,
-            id: json['id'],
-            text: json['text'],
-            user: json['user']);
-      }, first: (json) {
-        return threadTile(context, theme,
-            id: json['id'],
-            title: json['title'],
-            text: json['text'],
-            user: json['user']);
-      }, key: 'posts'),
+      body: ListApi.get(
+          "/threads/" + args['id'],
+          (json) {
+            return postTile(context, theme,
+                id: json['id'], text: json['text'], user: json['user']);
+          },
+          key: 'posts',
+          first: (json) {
+            return threadTile(context, theme,
+                id: json['id'],
+                title: json['title'],
+                text: json['text'],
+                user: json['user']);
+          },
+          last: (json) {
+            return threadReply(context, theme, id: json['id']);
+          }),
     );
   }
 }
 
-threadTile(context, theme, {String id, String title, String text, dynamic user}) {
+threadTile(context, theme,
+    {String id, String title, String text, dynamic user}) {
   var unescape = new HtmlUnescape();
   var logo = (user != null && user['avatar'] != null) ? user['avatar'] : null;
   if (title == null) title = '';
   if (text == null) text = '';
   return Card(
       child: ListTile(
-        leading: Image.network(logo + '@40x40'),
-        title: Text(unescape.convert(title)),
-        subtitle: Text(unescape.convert(text)),
-      ));
+    leading: Image.network(logo + '@40x40'),
+    title: Text(unescape.convert(title)),
+    subtitle: Text(unescape.convert(text)),
+  ));
 }
-
 
 postTile(context, theme, {String id, String text, dynamic user}) {
   var unescape = new HtmlUnescape();
-  var logo = (user != null && user['avatar'] != null) ? user['avatar'] + '@40x40' : "https://agroneo.net/ui/logo@64";
+  var logo = (user != null && user['avatar'] != null)
+      ? user['avatar'] + '@40x40'
+      : "https://agroneo.net/ui/logo@64";
   if (text == null) text = '';
   return Card(
       child: ListTile(
-        leading: Image.network(logo),
-        subtitle: Text(unescape.convert(text)),
-      ));
+    leading: Image.network(logo),
+    subtitle: Text(unescape.convert(text)),
+  ));
+}
+
+threadReply(context, theme, {String id}) {
+  return Card(
+      child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(children: <Widget>[TextField()])));
 }
