4e22ea0d4a7508ccbc04a4e83b5fa19809354fe2
pagetronic <pagetronic@gmail.com>
2020-01-15 18:25:21 +0400

    cleanup

--- src/main/java/com/agroneo/droid/threads/ForumsFragment.java
+++ src/main/java/com/agroneo/droid/threads/ForumsFragment.java
@@ -1,16 +1,24 @@
 package com.agroneo.droid.threads;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.agroneo.droid.R;
+import com.bumptech.glide.Glide;
+
+import live.page.android.api.Json;
+import live.page.android.views.ApiAdapter;
 
 public class ForumsFragment extends Fragment {
 
@@ -22,10 +30,42 @@ public class ForumsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
-        ForumAdapter forumAdapter = new ForumAdapter(getActivity(), R.layout.threads_view);
+        ForumAdapter forumAdapter = new ForumAdapter();
         forumAdapter.get("/questions?lng=fr");
         ((ListView) view.findViewById(R.id.threads)).setAdapter(forumAdapter);
         forumAdapter.notifyDataSetChanged();
     }
 
+    private class ForumAdapter extends ApiAdapter {
+
+        private ForumAdapter() {
+            super(getContext(), R.layout.threads_view);
+        }
+
+        @Override
+        public View getView(View convertView, final Json thread) {
+
+
+            ((TextView) convertView.findViewById(R.id.title)).setText(thread.getString("title"));
+            ((TextView) convertView.findViewById(R.id.text)).setText(thread.getString("text"));
+            Glide.with(context).load(Uri.parse(thread.getJson("user").getString("avatar") + "@64x64"))
+                    .error(R.drawable.logo)
+                    .into((ImageView) convertView.findViewById(R.id.avatar));
+
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context, ThreadsView.class);
+                    intent.putExtra("id", thread.getId());
+                    context.startActivity(intent);
+                }
+            });
+            return convertView;
+        }
+
+        protected Json getData(final Json data) {
+            return data.getJson("threads");
+        }
+    }
 }
