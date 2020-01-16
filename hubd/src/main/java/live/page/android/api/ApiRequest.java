c20f1d8f44a3ba3a0b4da5e37c8a04f64e0231c1
pagetronic <pagetronic@gmail.com>
2020-01-16 21:28:33 +0400

    Migrate live.page to lib module

---
+++ hubd/src/main/java/live/page/android/api/ApiRequest.java
@@ -0,0 +1,167 @@
+package live.page.android.api;
+
+import android.accounts.AccountManager;
+import android.content.Context;
+
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import live.page.android.BuildConfig;
+import live.page.android.R;
+
+public class ApiRequest {
+
+    private static final String HTTP = BuildConfig.DEBUG ? "http://" : "https://";
+    private Context ctx;
+    private HttpURLConnection connection;
+    private boolean abort = false;
+    private String access_token = null;
+
+    public ApiRequest(Context ctx, String path) {
+        this.ctx = ctx;
+        try {
+            connection = (HttpURLConnection) new URL(HTTP + ctx.getString(R.string.apiHost) + path).openConnection();
+            connection.setRequestProperty("User-Agent", "Agroneo TreePlace Android V3");
+            connection.setRequestProperty("Accept", "application/json");
+
+            if (BuildConfig.DEBUG) {
+                connection.setReadTimeout(Integer.MAX_VALUE);
+                connection.setConnectTimeout(Integer.MAX_VALUE);
+            } else {
+                connection.setReadTimeout(15000);
+                connection.setConnectTimeout(15000);
+            }
+            connection.setUseCaches(false);
+        } catch (IOException ignore) {
+        }
+    }
+
+    public void setAuthToken(String access_token) {
+        if (access_token != null) {
+            this.access_token = access_token;
+            connection.setRequestProperty("Authorization", "Bearer " + access_token);
+        }
+    }
+
+    public ApiResponse post(Json data) {
+
+        try {
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            OutputStream body = connection.getOutputStream();
+
+            BufferedWriter payload = new BufferedWriter(new OutputStreamWriter(body, StandardCharsets.UTF_8));
+            payload.write(data.toString(true));
+            payload.flush();
+            payload.close();
+
+            String response = null;
+            if (!isAbort()) {
+                response = read();
+            }
+
+            body.close();
+
+            if (!isAbort()) {
+                return new ApiResponse(connection.getResponseCode(), response);
+            }
+
+        } catch (IOException ignore) {
+        }
+        return null;
+    }
+
+    private String read() {
+
+        InputStream input = null;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            input = connection.getResponseCode() != HttpURLConnection.HTTP_OK ? connection.getErrorStream() : connection.getInputStream();
+            final int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            while (true) {
+                int count = input.read(buffer);
+                if (count == -1 || isAbort()) {
+                    break;
+                }
+                outputStream.write(buffer, 0, count);
+            }
+            if (!isAbort()) {
+                return new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
+            }
+
+        } catch (IOException ignore) {
+
+        } finally {
+
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (Exception ignore) {
+
+                }
+            }
+
+            try {
+                outputStream.close();
+            } catch (Exception ignore) {
+
+            }
+
+            try {
+                connection.disconnect();
+            } catch (Exception ignore) {
+
+            }
+
+        }
+        return null;
+    }
+
+
+    public ApiResponse get() {
+
+        try {
+
+            connection.setRequestMethod("GET");
+
+            String response = read();
+
+            if (!isAbort()) {
+                return new ApiResponse(connection.getResponseCode(), response);
+            }
+
+        } catch (IOException ignore) {
+        }
+        return null;
+    }
+
+
+    public void abort() {
+        abort = true;
+        try {
+            connection.disconnect();
+        } catch (Exception ignore) {
+        }
+    }
+
+
+    public boolean isAbort() {
+        return abort;
+    }
+
+    public void invalidateAuthToken() {
+        if (access_token != null) {
+            AccountManager am = AccountManager.get(ctx);
+            am.invalidateAuthToken(ctx.getString(R.string.account_type), access_token);
+        }
+    }
+}
---
+++ hubd/src/main/java/live/page/android/threads/ThreadsView.java
@@ -0,0 +1,94 @@
+package live.page.android.threads;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import live.page.android.R;
+import live.page.android.api.Json;
+import live.page.android.sys.PageActivity;
+import live.page.android.views.ApiAdapter;
+
+public class ThreadsView extends PageActivity {
+
+
+    private View firstView = null;
+    private View lastView = null;
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.thread;
+    }
+
+    @Override
+    protected void onCreate() {
+        final ThreadAdapter adapter = new ThreadAdapter();
+        ((ListView) findViewById(R.id.thread)).setAdapter(adapter);
+        adapter.get("/threads/" + getIntent().getStringExtra("id"));
+        final LayoutInflater inflater = getLayoutInflater();
+        firstView = inflater.inflate(R.layout.thread_post, new LinearLayout(this));
+        lastView = inflater.inflate(R.layout.thread_reply, new LinearLayout(this));
+
+    }
+
+    private class ThreadAdapter extends ApiAdapter {
+        private Json data = null;
+
+        private ThreadAdapter() {
+            super(ThreadsView.this, R.layout.thread_post);
+        }
+
+        @Override
+        public View getFirst() {
+            if (data == null) {
+                return new View(context);
+            }
+            return getView(firstView, data);
+        }
+
+        @Override
+        public View getLast() {
+            if (data == null) {
+                return new View(context);
+            }
+            return lastView;
+        }
+
+        @Override
+        public View getView(View convertView, final Json thread) {
+
+            ((TextView) convertView.findViewById(R.id.title)).setText(thread.getString("title"));
+            ((TextView) convertView.findViewById(R.id.text)).setText(thread.getString("text"));
+            Glide.with(context).load(Uri.parse(thread.getJson("user").getString("avatar") + "@64x64"))
+                    .error(R.drawable.logo)
+                    .into((ImageView) convertView.findViewById(R.id.avatar));
+
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(context, ThreadsView.class);
+                    intent.putExtra("id", thread.getId());
+                    context.startActivity(intent);
+                }
+            });
+            return convertView;
+        }
+
+        @Override
+        protected Json getData(final Json data) {
+
+            getSupportActionBar().setTitle(data.getString("title"));
+            this.data = data;
+            return data.getJson("posts");
+        }
+    }
+}
