71b3f649bfe3cbc5cb23ced3ad6ede12361ef2d2
pagetronic <pagetronic@gmail.com>
2020-01-21 15:48:50 +0400

    duplicate lng

--- agroneo/src/main/java/com/agroneo/droid/MainAgroneo.java
+++ agroneo/src/main/java/com/agroneo/droid/MainAgroneo.java
@@ -3,13 +3,12 @@ package com.agroneo.droid;
 import com.agroneo.droid.specimens.SpecimensFragment;
 
 import live.page.android.MainActivity;
-import live.page.android.sys.Settings;
 import live.page.android.threads.ForumsFragment;
 
 public class MainAgroneo extends MainActivity {
     @Override
     protected void init() {
-        MenuFragment questions = new MenuFragment("ROOT", "Questions", new ForumsFragment("/questions?lng=" + Settings.getLng(this)));
+        MenuFragment questions = new MenuFragment("ROOT", "Questions", new ForumsFragment("/questions"));
         MenuFragment specimens = new MenuFragment("SPECIMENS", "Specimens", new SpecimensFragment());
         addMenu(questions, specimens);
         loadFragment(questions);
--- renseigner/src/main/java/com/renseigner/droid/MainRenseigner.java
+++ renseigner/src/main/java/com/renseigner/droid/MainRenseigner.java
@@ -1,13 +1,12 @@
 package com.renseigner.droid;
 
 import live.page.android.MainActivity;
-import live.page.android.sys.Settings;
 import live.page.android.threads.ForumsFragment;
 
 public class MainRenseigner extends MainActivity {
     @Override
     protected void init() {
-        MenuFragment def = new MenuFragment("ROOT", "Questions", new ForumsFragment("/questions?lng=" + Settings.getLng(this)));
+        MenuFragment def = new MenuFragment("ROOT", "Questions", new ForumsFragment("/questions"));
         addMenu(def);
         loadFragment(def);
 
